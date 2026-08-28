88662e6d6d73f45a054f915c3a915034bdb17f53
Nandha K <nandhakumar@testpress.in>
2026-08-28 12:02:01 +0530

    Add superuser creation script to build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,16 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create or update superuser on deploy
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+u, created = User.objects.get_or_create(username='admin', defaults={'email': 'admin@example.com'})
+u.set_password('AdminPass123!')
+u.is_superuser = True
+u.is_staff = True
+u.is_active = True
+u.save()
+print('Superuser admin ready')
+"
